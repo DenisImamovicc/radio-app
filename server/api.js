4410b2e34ca1d6f46b8858529a1c805bc826f9ce
Denis Imamovic <Danielarbete@protonmail.com>
2023-04-26 13:55:31 +0200

    fix func refrence

--- server/api.js
+++ server/api.js
@@ -8,7 +8,7 @@ const JSON_FORMAT = `format=json`;
 
 function getAllList(req, res) {
   const TYPE = req.originalUrl;
-  fetch(SVERIGES_RADIO_API + TYPE + "?"+JSON_FORMAT)
+  fetch(SVERIGES_RADIO_API + TYPE + "?" + JSON_FORMAT)
     .then((res) => res.json())
     .then((Data) => res.status(200).send(Data))
     .catch(function (err) {
@@ -17,14 +17,16 @@ function getAllList(req, res) {
     });
 }
 
-function getProgramsFromChannel(req,res,id) {
-  fetch(`http://api.sr.se/api/v2/programs/index?channelid=${id}`+ "&"+ JSON_FORMAT)
-  .then((res) => res.json())
-  .then((Data) => res.status(200).send(Data))
-  .catch(function (err) {
-    console.error("sent from catch", err.message);
-    throw new Error(res.status(500).send(err));
-  });
+function getProgramsFromChannel(req, res, id) {
+  fetch(
+    `http://api.sr.se/api/v2/programs/index?channelid=${id}` + "&" + JSON_FORMAT
+  )
+    .then((res) => res.json())
+    .then((Data) => res.status(200).send(Data))
+    .catch(function (err) {
+      console.error("sent from catch", err.message);
+      throw new Error(res.status(500).send(err));
+    });
 }
 
 api.get("/channels", (req, res) => {
@@ -36,13 +38,12 @@ api.get("/programs", (req, res) => {
 });
 
 api.get("/programcategories", (req, res) => {
-    getAllList(req, res);
-  });
+  getAllList(req, res);
+});
 
-  api.get("/programsFromChannel/:id", (req, res) => {
-    let id = req.params.id
-    getSumthing(req, res , id)
-  });
-
+api.get("/programsFromChannel/:id", (req, res) => {
+  let id = req.params.id;
+  getProgramsFromChannel(req, res, id);
+});
 
 api.listen(port, () => console.log(port, `Live at http://localhost:${port}`));
