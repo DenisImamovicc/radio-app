87a97bc09ac68805c9b141d21aea453ddc00cc6b
Denis Imamovic <Danielarbete@protonmail.com>
2023-04-27 11:27:20 +0200

    fix and refactor code for communication between client and api

--- server/api.js
+++ server/api.js
@@ -17,11 +17,8 @@ function getAllList(req, res) {
     });
 }
 
-function getProgramsFromChannel(req, res, id) {
-  const TYPE = `req.originalUrl`
-  fetch(
-    `${SVERIGES_RADIO_API}/programs/index?channelid=${id}` + "&" + JSON_FORMAT
-  )
+function getDataById(req, res, id, params) {
+  fetch(SVERIGES_RADIO_API + params + `=${id}` + "&" + JSON_FORMAT)
     .then((res) => res.json())
     .then((Data) => res.status(200).send(Data))
     .catch(function (err) {
@@ -30,56 +27,29 @@ function getProgramsFromChannel(req, res, id) {
     });
 }
 
+api.get("/programscategorie/:id", (req, res) => {
+  let id = req.params.id;
+  let params = "/programs/index?programcategoryid";
+  getDataById(req, res, id, params);
+});
+api.get("/schedulechannel/:id", (req, res) => {
+  let id = req.params.id;
+  let params = "/scheduledepisodes?channelid";
+  getDataById(req, res, id, params);
+});
+
+api.get("/programschannel/:id", (req, res) => {
+  let id = req.params.id;
+  let params = "/programs/index?channelid";
+  getDataById(req, res, id, params);
+});
+
 api.get("/channels", (req, res) => {
   getAllList(req, res);
 });
 
-api.get("/categories", (req, res) => {
+api.get("/programcategories", (req, res) => {
   getAllList(req, res);
-});
-
-api.get("/channelid/:id", (req, res) => {
-  let id = req.params.id;
-  getProgramsFromChannel(req, res, id);
-});
-api.get("/programcategoryid/:id", (req, res) => {
-   let id = req.params.id;
-  // getProgramsFromChannel(req, res, id);
-
-  fetch(
-    `${SVERIGES_RADIO_API}/programs/index?programcategoryid=${id}` + "&" + JSON_FORMAT
-  )
-    .then((res) => res.json())
-    .then((Data) => res.status(200).send(Data))
-    .catch(function (err) {
-      console.error("sent from catch", err.message);
-      throw new Error(res.status(500).send(err));
-    });
-});
-api.get("/scheduledepisode/:id", (req, res) => {
-  let id = req.params.id;
-  fetch(
-    `${SVERIGES_RADIO_API}/scheduledepisodes?channelid=${id}` + "&" + JSON_FORMAT
-  )
-    .then((res) => res.json())
-    .then((Data) => res.status(200).send(Data))
-    .catch(function (err) {
-      console.error("sent from catch", err.message);
-      throw new Error(res.status(500).send(err));
-    });
-});
-
-api.get("/broadcastsfromprogram/:id", (req, res) => {
-  let id = req.params.id;
-  fetch(
-    `${SVERIGES_RADIO_API}/programs/index?channelid=${id}` + "&" + JSON_FORMAT
-  )
-    .then((res) => res.json())
-    .then((Data) => res.status(200).send(Data))
-    .catch(function (err) {
-      console.error("sent from catch", err.message);
-      throw new Error(res.status(500).send(err));
-    });
 });
 
 api.listen(port, () => console.log(port, `Live at http://localhost:${port}`));
