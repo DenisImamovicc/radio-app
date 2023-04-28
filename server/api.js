00116d9052c4e54461722a324d29731bf4836b15
Denis Imamovic <Danielarbete@protonmail.com>
2023-04-28 13:49:36 +0200

    add simple post req to api for storing user data in sql db

--- server/api.js
+++ server/api.js
@@ -1,10 +1,40 @@
 import express from "express";
 import fetch from "node-fetch";
+import bodyParser from "body-parser"
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
 const port = 9000;
 const api = express();
 const SVERIGES_RADIO_API = `http://api.sr.se/api/v2`;
 const JSON_FORMAT = `format=json`;
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+
+
+// const DB = open({  filename: 'C:/Users/Work/Desktop/sqldbs/Radiouseracount.db', driver: sqlite3.Database}).then((db)=>{
+//   return db
+// })
+
+const db = new sqlite3.Database('C:/Users/Work/Desktop/sqldbs/Radiouseracount.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
+
+db.all('SELECT * FROM Useracounts', (err, rows) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log(rows);
+  }
+});
+
+console.log(db);
+
 
 function getAllList(req, res) {
   const TYPE = req.originalUrl;
@@ -27,11 +57,27 @@ function getDataById(req, res, id, params) {
     });
 }
 
+api.post("/newacount", (req, res) => {
+  const data = req.body
+  console.log(data);
+  db.run('INSERT INTO Useracounts (Email, Password) VALUES (?, ?)', [data.Email, data.Password], function(err) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    }
+  });
+  
+  res.sendStatus(200)
+
+});
+
 api.get("/programscategorie/:id", (req, res) => {
   let id = req.params.id;
   let params = "/programs/index?programcategoryid";
   getDataById(req, res, id, params);
 });
+
 api.get("/schedulechannel/:id", (req, res) => {
   let id = req.params.id;
   let params = "/scheduledepisodes?channelid";
