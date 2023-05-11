375a3a6c1be7dd06095e42387840620c441bd481
Denis Imamovic <Danielarbete@protonmail.com>
2023-05-11 13:30:53 +0200

    route chaining

--- server/routes/SR_API_route.js
+++ server/routes/SR_API_route.js
@@ -1,24 +1,20 @@
 import express from "express";
 import { getDataById, getAllList } from "../utils/functions.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/programscategorie/:id", (req, res) =>
-  getDataById(req, res, req.params.id, "/programs/index?programcategoryid")
-);
+router
+  .get("/programscategorie/:id", (req, res) =>
+    getDataById(req, res, req.params.id, "/programs/index?programcategoryid")
+  )
+  .get("/schedulechannel/:id", (req, res) =>
+    getDataById(req, res, req.params.id, "/scheduledepisodes?channelid")
+  )
+  .get("/programschannel/:id", (req, res) =>
+    getDataById(req, res, req.params.id, "/programs/index?channelid")
+  )
+  .get("/channels", (req, res) => getAllList(req, res))
+  .get("/channels/:id", (req, res) => getAllList(req, res))
+  .get("/programcategories", (req, res) => getAllList(req, res));
 
-router.get("/schedulechannel/:id", (req, res) =>
-  getDataById(req, res, req.params.id, "/scheduledepisodes?channelid")
-);
-
-router.get("/programschannel/:id", (req, res) =>
-  getDataById(req, res, req.params.id, "/programs/index?channelid")
-);
-
-router.get("/channels", (req, res) => getAllList(req, res));
-
-router.get("/channels/:id", (req, res) => getAllList(req, res));
-
-router.get("/programcategories", (req, res) => getAllList(req, res));
-
-export default router
+export default router;
