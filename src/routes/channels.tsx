5b71523409b601f87ab29622acbb185eaa7691cf
Denis Imamovic <Danielarbete@protonmail.com>
2023-05-17 20:21:03 +0200

    generate cards with dynamic values for channel and program list

--- src/routes/channels.tsx
+++ src/routes/channels.tsx
@@ -1,9 +1,24 @@
+import data from "../ChannelData.json";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function Channels() {
+const Channels = () => {
+  const channels = data.channels;
+  console.log(channels);
+  
   return (
     <>
-    <h1>channels</h1>
+      {channels.map((channel) => (
+          <Card key={channel.id} className="m-3">
+            <Card.Img variant="top" src={channel.image} />
+            <Card.Body>
+              <Card.Title>{channel.name}-{channel.channeltype}</Card.Title>
+              <Card.Text>{channel.tagline}</Card.Text>
+              <Button variant="primary" >Play latest</Button>
+            </Card.Body>
+          </Card>
+      ))}
     </>
-  )
+  );
 }
-export default Channels
+export default Channels;
--- src/routes/programs.tsx
+++ src/routes/programs.tsx
@@ -1,8 +1,22 @@
+import data from "../ProgramsData.json";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Programs() {
+  const programs = data.programs;
+  console.log(programs);
   return (
     <>
-    <h1>Programs</h1>
+      {programs.map((program) => (
+          <Card key={program.id} className="m-3">
+            <Card.Img variant="top" src={program.programimage} />
+            <Card.Body>
+              <Card.Title>{program.name}-{program.programcategory?.name}</Card.Title>
+              <Card.Text>{program.description}</Card.Text>
+              <Button variant="primary" >Play latest</Button>
+            </Card.Body>
+          </Card>
+      ))}
     </>
   )
 }
