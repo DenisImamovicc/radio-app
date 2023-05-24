aebd80135480ef5694b1cf301107c447d1f65521
Denis Imamovic <Danielarbete@protonmail.com>
2023-05-24 16:32:13 +0200

    enable basic pwa for installing application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA({ 
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Sveriges Radio',
+      short_name: 'SRadio',
+      description: 'Radio/Tablå för Sveriges Radio',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-192.maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "icons/manifest-icon-512.maskable.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  })],
 })
