77ddcf661f8dfd3b7a9952ce72df5ca01f56b4d5
Denis Imamovic <Danielarbete@protonmail.com>
2023-06-01 19:30:28 +0200

    add visual confirmation with dynamic names when user favorites/unfavorites a channel/program

--- src/components/Favoriteicon.tsx
+++ src/components/Favoriteicon.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
+import Toast from "react-bootstrap/Toast";
 
 interface channel {
   id: number;
@@ -15,19 +16,22 @@ interface channel {
 }
 
 const ToggleIcon = ({ channel }: channel) => {
-  const [isClicked, setIsClicked] = useState(localStorage.getItem(`${channel.name} isFav?`));
+  const [isClicked, setIsClicked] = useState(
+    localStorage.getItem(`${channel.name} isFav?`)
+  );
+  const [ShowToast, setShowToast] = useState(false);
 
   useEffect(() => {
-     if (localStorage.getItem(`${channel.name} isFav?`) === "false") {
-         console.log("useffect");
-         setIsClicked(false);
-     }
-  }, [isClicked])
-  
+    if (localStorage.getItem(`${channel.name} isFav?`) === "false") {
+      console.log("useffect");
+      setIsClicked(false);
+    }
+  }, [isClicked]);
 
   const handleClick = () => {
     setIsClicked(!isClicked);
     localStorage.setItem(`${channel.name} isFav?`, `${!isClicked}`);
+    setShowToast(true);
   };
   //   console.log(channel);
 
@@ -38,6 +42,20 @@ const ToggleIcon = ({ channel }: channel) => {
         onClick={handleClick}
         className="ms-1"
       />
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={ShowToast}
+        delay={3000}
+        className="toastFavorite"
+        bg="dark"
+      >
+        <Toast.Header >
+          {/* <img src={channel.image} className=" me-2 " alt="" /> */}
+          <strong className="me-auto">
+            {channel.name} har {isClicked ? "favoritmarkerats!":"avfavoritmarkerats!"}
+          </strong>
+        </Toast.Header>
+      </Toast>
     </>
   );
 };
