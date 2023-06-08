ce7cc9d091a2d96e766da5eef83ba97d3823a460
Denis Imamovic <Danielarbete@protonmail.com>
2023-06-08 15:52:24 +0200

    Add dynamic dropdown titel on current category

--- src/components/ProgramTypeDropdown.tsx
+++ src/components/ProgramTypeDropdown.tsx
@@ -1,6 +1,7 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import useFetch from "../hooks/useFetch";
 import Loadingprogramcard from "./Loadingprogramcard";
+import {useState} from "react"
 
 interface ProgramTypeDropdown {
   setprogramCategory: (categoryId: string) => void;
@@ -11,6 +12,7 @@ const ProgramTypeDropdown = ({
   setprogramCategory,
   setUrl,
 }: ProgramTypeDropdown) => {
+  const [titel, settitel] = useState("Kategori")
   const { data } = useFetch(
     "https://api.sr.se/api/v2/programcategories?format=json&size=15"
   );
@@ -19,26 +21,26 @@ const ProgramTypeDropdown = ({
     return <Loadingprogramcard />;
   }
 
-  const filterProgramsByCategory = (categoryId: string) => {
-    setprogramCategory(categoryId);
+  const handleOnClick = (category: any) => {
+    setprogramCategory(category.id);
     setUrl(
-      `https://api.sr.se/api/v2/programs?programcategoryid=${categoryId}&format=json`
+      `https://api.sr.se/api/v2/programs?programcategoryid=${category.id}&format=json`
     );
+    settitel(category.name)
   };
 
   return (
     <>
       <Dropdown className="m-3">
         <Dropdown.Toggle id="dropdown-basic" variant="dark">
-          Kategori
+          {titel}
         </Dropdown.Toggle>
         <Dropdown.Menu variant="dark">
           {data.programcategories &&
             data.programcategories.map((category: any) => (
               <Dropdown.Item
-                href=""
                 key={category.id}
-                onClick={() => filterProgramsByCategory(category.id)}
+                onClick={() => handleOnClick(category)}
               >
                 {category.name}
               </Dropdown.Item>
