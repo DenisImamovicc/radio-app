17c6d7735e4c2d186a191f62e52c9867a5c34f28
Denis Imamovic <Danielarbete@protonmail.com>
2023-06-12 15:56:00 +0200

    responsive design channel schedule and programs

--- src/routes/Channeloptions.tsx
+++ src/routes/Channeloptions.tsx
@@ -7,6 +7,10 @@ import PaginationComponent from "../components/PaginationComponent.tsx";
 import { useState } from "react";
 import Loadingprogramcard from "../components/Loadingprogramcard.tsx";
 
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 interface Channeloptions {
   channelData: {
     id: number;
@@ -14,7 +18,9 @@ interface Channeloptions {
   };
 }
 
-export default function Channeloptions({ channelData: { id, scheduleurl }}: Channeloptions) {
+export default function Channeloptions({
+  channelData: { id, scheduleurl },
+}: Channeloptions) {
   const [urls, setUrls] = useState({
     channelUrl: `${scheduleurl}&format=json`,
     programUrl: `https://api.sr.se/api/v2/programs/index?format=json&channelid=${id}`,
@@ -22,7 +28,7 @@ export default function Channeloptions({ channelData: { id, scheduleurl }}: Chan
 
   const { data: broadCastData } = useFetch(urls.channelUrl);
   const { data: programsData } = useFetch(urls.programUrl);
-  
+
   const handleFetchNextPage = (nextpageData: string) => {
     setUrls((prevUrls) => {
       if (nextpageData.includes("programs")) {
@@ -45,19 +51,31 @@ export default function Channeloptions({ channelData: { id, scheduleurl }}: Chan
         className="mb-3"
       >
         <Tab eventKey="Sändningar" title="Sändningar">
-          {broadCastData.schedule &&
-            broadCastData.schedule.map((episode: any,index: number) => (
-              <Broadcastcard episode={episode} key={index}/>
-            ))}
+          <Container>
+            <Row xs={1} md={2} lg={3}>
+              {broadCastData.schedule &&
+                broadCastData.schedule.map((episode: any, index: number) => (
+                  <Col>
+                    <Broadcastcard episode={episode} key={index} />
+                  </Col>
+                ))}
+            </Row>
+          </Container>
           <PaginationComponent
             data={broadCastData.pagination}
             handleFetchNextPage={handleFetchNextPage}
           />
         </Tab>
         <Tab eventKey="Program" title="Program">
-          {programsData.programs.map((program: any,index: number) => (
-            <ProgramCard program={program} key={index}/>
-          ))}
+          <Container>
+            <Row xs={1} md={2} lg={3}>
+              {programsData.programs.map((program: any, index: number) => (
+                <Col>
+                  <ProgramCard program={program} key={index} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
           <PaginationComponent
             data={programsData.pagination}
             handleFetchNextPage={handleFetchNextPage}
