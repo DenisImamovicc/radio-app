839de182d64f70c75428ae3d1237fd3da4626cdd
Denis Imamovic <Danielarbete@protonmail.com>
2023-06-12 15:36:50 +0200

    responsive design layout on din sida

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -4,6 +4,10 @@ import Tabs from "react-bootstrap/Tabs";
 import ProgramCards from "../components/Programscard";
 import ChannelsCard from "../components/ChannelsCard";
 
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 interface User {
   id: number;
   image: string;
@@ -20,57 +24,81 @@ interface UserProps {
 }
 
 interface channel {
-    id: number;
-    image: string;
-    channeltype: string;
-    name: string;
-    tagline: string;
-    liveaudio: {
-      url: string;
-    };
+  id: number;
+  image: string;
+  channeltype: string;
+  name: string;
+  tagline: string;
+  liveaudio: {
+    url: string;
+  };
 }
 
 export default function User({ setaudioFile }: UserProps) {
-  const [favoriteChannels, setFavoriteChannels] = useState<User[]|null>(null);
-  const [favoritePrograms, setFavoritePrograms] = useState<User[]|null>(null);
+  const [favoriteChannels, setFavoriteChannels] = useState<User[] | null>(null);
+  const [favoritePrograms, setFavoritePrograms] = useState<User[] | null>(null);
 
   useEffect(() => {
-    let channelData:User[] | null = JSON.parse(localStorage.getItem("FavoriteChannelsList")|| "null");
-    channelData?.length ? setFavoriteChannels(channelData) : setFavoriteChannels(null)
+    let channelData: User[] | null = JSON.parse(
+      localStorage.getItem("FavoriteChannelsList") || "null"
+    );
+    channelData?.length
+      ? setFavoriteChannels(channelData)
+      : setFavoriteChannels(null);
 
-    let programData:User[] | null = JSON.parse(localStorage.getItem("FavoriteProgramsList")|| "null");
-    programData?.length ? setFavoritePrograms(programData) : setFavoritePrograms(null)
+    let programData: User[] | null = JSON.parse(
+      localStorage.getItem("FavoriteProgramsList") || "null"
+    );
+    programData?.length
+      ? setFavoritePrograms(programData)
+      : setFavoritePrograms(null);
   }, []);
 
   return (
-    <> 
+    <>
       <h1 className="p-1 text-light">Din sida</h1>
       <Tabs
         defaultActiveKey="Favorit kanaler"
         id="uncontrolled-tab-example"
         className="mb-3 "
       >
-        <Tab
-          eventKey="Favorit kanaler"
-          title="Favorit kanaler"
-          className=""
-        >
-          {favoriteChannels ? favoriteChannels.map((channel:channel)=>
-            <ChannelsCard channel={channel} setaudioFile={setaudioFile}/>
-          ) : (
-            <div className="user-nocontent">Inga Favoritmarkerade kanaler än :)</div>
-          )}
+        <Tab eventKey="Favorit kanaler" title="Favorit kanaler" className="">
+          <Container>
+            <Row xs={1} md={2} lg={3}>
+              {favoriteChannels ? (
+                favoriteChannels.map((channel: channel) => (
+                  <Col>
+                    <ChannelsCard
+                      channel={channel}
+                      setaudioFile={setaudioFile}
+                    />
+                  </Col>
+                ))
+              ) : (
+                <div className="user-nocontent">
+                  Inga Favoritmarkerade kanaler än :)
+                </div>
+              )}
+            </Row>
+          </Container>
         </Tab>
-         <Tab
-          eventKey="Favorit program"
-          title="Favorit program"
-        >
-          {favoritePrograms ? favoritePrograms.map((program:any)=>
-            <ProgramCards program={program}/>
-          ) : (
-            <div className="user-nocontent">Inga Favoritmarkerade program än :)</div>
-          )}
-          </Tab>
+        <Tab eventKey="Favorit program" title="Favorit program">
+          <Container>
+            <Row xs={1} md={2} lg={3}>
+              {favoritePrograms ? (
+                favoritePrograms.map((program: any) => (
+                  <Col>
+                    <ProgramCards program={program} />
+                  </Col>
+                ))
+              ) : (
+                <div className="user-nocontent">
+                  Inga Favoritmarkerade kanaler än :)
+                </div>
+              )}
+            </Row>
+          </Container>
+        </Tab>
       </Tabs>
     </>
   );
