dcd6f401818dd54a7f466ae06122878fc787f157
Denis Imamovic <Danielarbete@protonmail.com>
2023-07-07 13:40:03 +0200

    log db when starting up

--- server/config/SQL_DB.js
+++ server/config/SQL_DB.js
@@ -12,5 +12,28 @@ export const DB = new sqlite3.Database(dbPath, (err) => {
     console.error(err.message);
   } else {
     console.log("Connected to the SQLite database.");
+    logDatabase();
   }
 });
+
+function logDatabase() {
+  DB.all("SELECT name FROM sqlite_master WHERE type='table'", (err, tables) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+
+    tables.forEach((table) => {
+      const tableName = table.name;
+      DB.all(`SELECT * FROM ${tableName}`, (err, rows) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+
+        console.log(`Table: ${tableName}`);
+        console.log(rows);
+      });
+    });
+  });
+}
