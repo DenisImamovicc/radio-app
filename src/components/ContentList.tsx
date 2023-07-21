2474396cb376d60e6a3f84461a18e0e83afe34ae
Denis Imamovic <Danielarbete@protonmail.com>
2023-07-21 14:26:30 +0200

    Add download icon and functionality for only episodes,may make for the rest of media in a programme.

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import useFetch from "../hooks/useFetch";
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faPlay } from "@fortawesome/free-solid-svg-icons";
 import PaginationComponent from "./PaginationComponent";
 
 interface data {
@@ -32,7 +32,6 @@ function ContentList({ data, setaudioFile, contentType }: data) {
   const [Url, setUrl] = useState(
     `https://api.sr.se/api/v2/${contentType}?programid=${data.id}&format=json`
   );
-
   const { data: contentData } = useFetch(Url);
 
   const handleFetchNextPage = (nextpageData: string) => {
@@ -41,7 +40,6 @@ function ContentList({ data, setaudioFile, contentType }: data) {
 
   const playAudio = (url: string) => setaudioFile(url);
 
-  //Avsnitt data struktur är fucky emellan olika program.Figure it out
   const handleContentTypePath = (content: any) => {
     if (contentType === "broadcasts") {
       return content.broadcastfiles[0].url;
@@ -72,44 +70,61 @@ function ContentList({ data, setaudioFile, contentType }: data) {
     return 0;
   };
 
+  const handleDownloadUrl = (data: any) => data.downloadpodfile?.urls || "#";
+
   if (!contentData) {
-    return  <Spinner animation="border" variant="light"/>
+    return <Spinner animation="border" variant="light" />;
   }
 
   if (!contentData[contentType].length) {
-    return  <h2 className="text-light text-center" id="nodatatitel">Hittat ingen tillgänglig data</h2>
+    return (
+      <h2 className="text-light text-center" id="nodatatitel">
+        Hittat ingen tillgänglig data
+      </h2>
+    );
   }
-  // console.log(contentData);
 
   return (
     <>
       <ListGroup as="ol" numbered className="mb-3" id="Contentlist">
-        {contentData && contentData[contentType] ? (
-          contentData[contentType].map((content: any) => (
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start mx-3  text-light bg-dark"
-              key={content.id}
-            >
-              <div className="ms-2 me-auto ">
-                <div className="fw-bold">{content.title}</div>
-                {content.description ? <p>{content.description}</p> : ""}
-                <p>
-                  <FontAwesomeIcon
-                    onClick={() => playAudio(handleContentTypePath(content))}
-                    className="me-1"
-                    icon={faPlay}
-                  />
-                  {Math.round((handleContentDurationPath(content) / 60) * 10) /
-                    10}
-                  min
-                </p>
-              </div>
-            </ListGroup.Item>
-          ))
-        ) : (
-          ""
-        )}
+        {contentData && contentData[contentType]
+          ? contentData[contentType].map((content: any) => (
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start mx-3  text-light bg-dark"
+                key={content.id}
+              >
+                <div className="ms-2 me-auto ">
+                  <div className="fw-bold">
+                    {content.title}
+                    {contentType === "episodes" ? (
+                      <a
+                        href={handleDownloadUrl(content)}
+                        className="text-light ms-1"
+                        id="downloadComponent"
+                      >
+                        <FontAwesomeIcon icon={faDownload} />
+                      </a>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  {content.description ? <p>{content.description}</p> : ""}
+                  <p>
+                    <FontAwesomeIcon
+                      onClick={() => playAudio(handleContentTypePath(content))}
+                      className="me-1"
+                      icon={faPlay}
+                    />
+                    {Math.round(
+                      (handleContentDurationPath(content) / 60) * 10
+                    ) / 10}
+                    min
+                  </p>
+                </div>
+              </ListGroup.Item>
+            ))
+          : ""}
       </ListGroup>
       <PaginationComponent
         data={contentData.pagination}
